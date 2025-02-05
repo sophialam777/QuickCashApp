237d2c52f98d2ada6ac5cc10722e7f3d08e09864
msajan <mh319326@dal.ca>
2025-02-05 14:58:48 -0400

    Reg fix

---
+++ app/src/main/java/com/example/iteration1/Registration.java
@@ -0,0 +1,17 @@
+package com.example.iteration1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+public class Registration extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registration); // Links to the registration page layout
+
+        // Your registration logic here
+    }
+}
