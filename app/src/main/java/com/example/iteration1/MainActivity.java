60b01baa8c7c8b9256c817d299813bef34cfb1ac
Matthew Fraser <mt769340@dal.ca>
2025-02-10 14:55:50 -0400

    clearing un-used files

--- app/src/main/java/com/example/iteration1/MainActivity.java
+++ app/src/main/java/com/example/iteration1/MainActivity.java
@@ -1,13 +1,6 @@
 package com.example.iteration1;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,14 +8,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+public class MainActivity extends AppCompatActivity{
 
-    private EditText email;
-    private EditText password;
-    private Spinner roleSpinner;
-    private String role;
-    private String[] roleOptions = {"Select a role", "Employee", "Employer"};
-    private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,30 +21,5 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        initializeLoginElements();
-        initializeOnClickListener();
-    }
-
-    private void initializeLoginElements(){
-        email = findViewById(R.id.login_email);
-        password = findViewById(R.id.login_password);
-        roleSpinner = findViewById(R.id.login_role);
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, roleOptions);
-        roleSpinner.setAdapter(arrayAdapter);
-        roleSpinner.setOnItemSelectedListener((AdapterView.OnItemSelectedListener)this);
-    }
-
-    private void initializeOnClickListener(){
-        login = findViewById(R.id.login_button);
-    }
-
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        role = roleOptions[position];
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
     }
 }
