2d65c39567e7b68a187b6677378e7f6626555617
Msajan <mh319326@dal.ca>
2025-02-11 14:48:43 -0400

    Added Forgot Password

--- app/src/main/java/com/example/iteration1/Login.java
+++ app/src/main/java/com/example/iteration1/Login.java
@@ -1,5 +1,6 @@
 package com.example.iteration1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -7,6 +8,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -27,7 +29,7 @@ public class Login extends AppCompatActivity implements AdapterView.OnItemSelect
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -46,8 +48,22 @@ public class Login extends AppCompatActivity implements AdapterView.OnItemSelect
         roleSpinner.setOnItemSelectedListener((AdapterView.OnItemSelectedListener)this);
     }
 
-    private void initializeOnClickListener(){
+    private void initializeOnClickListener() {
         login = findViewById(R.id.login_button);
+        Button registerButton = findViewById(R.id.register_link); // The Register button in your layout
+        TextView forgotPasswordText = findViewById(R.id.forgotPassword); // The Forgot Password text in your layout
+
+        // Register Button click listener
+        registerButton.setOnClickListener(v -> {
+            Intent intent = new Intent(Login.this, Registration.class);
+            startActivity(intent);
+        });
+
+        // Forgot Password click listener
+        forgotPasswordText.setOnClickListener(v -> {
+            Intent intent = new Intent(Login.this, ForgotPassword.class);
+            startActivity(intent);
+        });
     }
 
     @Override
@@ -59,4 +75,5 @@ public class Login extends AppCompatActivity implements AdapterView.OnItemSelect
     public void onNothingSelected(AdapterView<?> parent) {
 
     }
+
 }
