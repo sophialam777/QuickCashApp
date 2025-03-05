e9e31172af8093268e33d809eed6b1d46a214777
Msajan <mh319326@dal.ca>
2025-03-05 02:07:25 -0400

    Updated JobListDetails test

--- app/src/test/java/com/example/iteration1/JobListDetailsTest.java
+++ app/src/test/java/com/example/iteration1/JobListDetailsTest.java
@@ -10,12 +10,13 @@ public class JobListDetailsTest {
     @Test
     public void testJobGetters() {
         Job job = new Job("Software Engineer", "Develop software", "Java, Kotlin", "Submit resume", 44.6452, -63.5736);
+        double delta = 0.0001;
 
         assertEquals("Software Engineer", job.getTitle());
         assertEquals("Develop software", job.getDescription());
         assertEquals("Java, Kotlin", job.getRequirements());
         assertEquals("Submit resume", job.getInstructions());
-        assertEquals(44.6452, job.getLatitude());
-        assertEquals(-63.5736, job.getLongitude());
+        assertEquals(44.6452, job.getLatitude(), delta);
+        assertEquals(-63.5736, job.getLongitude(), delta);
     }
 }
